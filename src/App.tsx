29af935982d3c1e59514d31915da266aa69a4b0f
xbdrc <contact.brunocruz@gmail.com>
2026-09-13 14:57:18 +0100

    update texts

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
         <h1>React MNIST</h1>
         <text><a href="https://en.wikipedia.org/wiki/MNIST_database" target="_blank">MNIST</a> is a large database of handwritten digits from 0 to 9. It is commonly used for training purposes of various image preprocessing systems.</text>
         <text>To test the machine learning model trained with MNIST, <b>you can draw a digit on the black board.</b></text>
-        {/* <text style={{ color: "lightgray", fontSize: 10 }}>The model is trained and tested with ~87% accuracy. There is still a chance the prediction comes incorrect.</text> */}
+        <text style={{ color: "lightgray", fontSize: 10 }}>The model is trained and tested with ~87% accuracy. There is still a chance the prediction comes incorrect.</text>
       </div>
       <div className='board'>
         <MNIST />
